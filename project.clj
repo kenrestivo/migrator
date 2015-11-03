47453cb14e606b45756c0a1beae46176df27ff33
ken restivo <ken@restivo.org>
2015-11-03 12:02:47 -0800

    Add basic requirements

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [cheshire "5.5.0"]
+                 [clj-http "2.0.0"]
+                 ;; [org.clojure/data.xml "0.0.8"]
+                 ])
